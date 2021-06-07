740a221c3fee26ac873aa8d8feac183da153645f
Vinta Chen <vinta.chen@gmail.com>
2021-06-07 16:01:02 +0800

    add Uniswap test helper

---
+++ contracts/test/TestERC20.sol
@@ -0,0 +1,6 @@
+pragma solidity 0.8.4;
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract TestERC20 is ERC20 {
+    constructor(string memory name, string memory symbol) ERC20(name, symbol) {}
+}
