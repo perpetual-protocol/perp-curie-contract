91b9caa24d56cf566a4fd2ed78f09f617dfb96d3
Vinta Chen <vinta.chen@gmail.com>
2021-06-07 16:36:06 +0800

    uncomment sample tests

--- test/test.ts
+++ test/test.ts
@@ -1,18 +1,18 @@
-// import { expect } from "chai"
-// import { ethers } from "hardhat"
-// import { Sample } from "../typechain"
+import { expect } from "chai"
+import { ethers } from "hardhat"
+import { Sample } from "../typechain"
 
-// describe("sample", () => {
-//     it("sample test", async () => {
-//         const Sample = await ethers.getContractFactory("Sample")
-//         const sample = (await Sample.deploy()) as Sample
+describe("sample", () => {
+    it("sample test", async () => {
+        const Sample = await ethers.getContractFactory("Sample")
+        const sample = (await Sample.deploy()) as Sample
 
-//         await sample.deployed()
-//         const prev = await sample.c()
-//         expect(prev.toString()).eq("0")
-//         const tx = await sample.test(5, 2)
-//         await tx.wait()
-//         const now = await sample.c()
-//         expect(now.toString()).eq("7")
-//     })
-// })
+        await sample.deployed()
+        const prev = await sample.c()
+        expect(prev.toString()).eq("0")
+        const tx = await sample.test(5, 2)
+        await tx.wait()
+        const now = await sample.c()
+        expect(now.toString()).eq("7")
+    })
+})
