bcfa79e9a55537e44b87f1c27e960593c62e3a81
kimiwu123 <leonhartwu@gmail.com>
2021-06-07 17:07:06 +0800

    feat: addPool added

--- contracts/ClearingHouse.sol
+++ contracts/ClearingHouse.sol
@@ -1,11 +1,24 @@
 pragma solidity 0.8.4;
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IUniswapV3Factory } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
+import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
+import { UniswapV3Broker } from "./lib/UniswapV3Broker.sol";
 
-contract ClearingHouse {
+contract ClearingHouse is Ownable {
+    //
+    // events
+    //
+    event PoolAdded(address pool);
+
+    //
+    // state variables
+    //
     IERC20 public quoteAsset;
     IERC20 public vQuoteAsset;
     IUniswapV3Factory public uniV3Factory;
+
+    mapping(IUniswapV3Pool => bool) poolMap;
 
     constructor(
         IERC20 vQuoteAssetParam,
@@ -15,5 +28,17 @@ contract ClearingHouse {
         vQuoteAsset = vQuoteAssetParam;
         quoteAsset = quoteAssetParam;
         uniV3Factory = uniV3FactoryParam;
+    }
+
+    function addPool(IUniswapV3Pool pool) external onlyOwner {
+        // CH_EP: existent pool in ClearingHouse
+        require(!poolMap[pool], "CH_EP");
+        // CH_NEP: non-existent pool in uniV3 factory
+        require(UniswapV3Broker.isExistedPool(address(uniV3Factory), address(pool)), "CH_NEP");
+
+        // update poolMap
+        poolMap[pool] = true;
+
+        emit PoolAdded(address(pool));
     }
 }
--- contracts/lib/UniswapV3Broker.sol
+++ contracts/lib/UniswapV3Broker.sol
@@ -1,119 +1,88 @@
-pragma solidity 0.7.6;
-pragma abicoder v2;
-
+pragma solidity 0.8.4;
 import { IUniswapV3Pool } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
-import { TickMath } from "@uniswap/v3-core/contracts/libraries/TickMath.sol";
-import { PositionKey } from "@uniswap/v3-periphery/contracts/libraries/PositionKey.sol";
-import { LiquidityAmounts } from "@uniswap/v3-periphery/contracts/libraries/LiquidityAmounts.sol";
 
 library UniswapV3Broker {
-    struct MintParams {
-        IUniswapV3Pool pool;
-        address baseToken;
-        address quoteToken;
-        int24 tickLower;
-        int24 tickUpper;
-        uint256 baseAmount;
-        uint256 quoteAmount;
+    function addLiquidity(
+        address _pool,
+        int24 _tickLower,
+        int24 _tickUpper,
+        uint256 _base,
+        uint256 _quote
+    )
+        internal
+        returns (
+            uint256 base,
+            uint256 quote,
+            uint256 liquidityDelta,
+            uint256 feeGrowthInsideLastBase,
+            uint256 feeGrowthInsideLastQuote
+        )
+    {
+        revert();
     }
 
-    struct MintResponse {
-        uint256 base;
-        uint256 quote;
-        uint128 liquidity;
-        uint256 feeGrowthInsideLastBase;
-        uint256 feeGrowthInsideLastQuote;
+    function removeLiquidity(
+        address _pool,
+        int24 _tickLower,
+        int24 _tickUpper,
+        uint256 _liquidity
+    )
+        internal
+        returns (
+            uint256 base,
+            uint256 quote,
+            uint256 liquidityDelta,
+            uint256 feeGrowthInsideLastBase,
+            uint256 ffeeGrowthInsideLastQuote
+        )
+    {
+        revert();
     }
 
-    function mint(MintParams memory params) internal returns (MintResponse memory response) {
-        // zero inputs
-        require(params.baseAmount > 0 || params.quoteAmount > 0, "UB_ZIs");
-
-        // make base & quote into the right order
-        bool isBase0Quote1 = _isBase0Quote1(params.pool, params.baseToken, params.quoteToken);
-        uint256 amount0;
-        uint256 amount1;
-        if (isBase0Quote1) {
-            amount0 = params.baseAmount;
-            amount1 = params.quoteAmount;
-        } else {
-            amount0 = params.quoteAmount;
-            amount1 = params.baseAmount;
-        }
-
-        // fetch the fee growth state if this has liquidity
-        uint256 feeGrowthInside0LastX128;
-        uint256 feeGrowthInside1LastX128;
-        if (_getPositionLiquidity(params.pool, params.tickLower, params.tickUpper) > 0) {
-            // get this' positionKey
-            // FIXME
-            // check if the case sensitive of address(this) break the PositionKey computing
-            bytes32 positionKey = PositionKey.compute(address(this), params.tickLower, params.tickUpper);
-
-            // get feeGrowthInside{0,1}LastX128
-            (, feeGrowthInside0LastX128, feeGrowthInside1LastX128, , ) = params.pool.positions(positionKey);
-        }
-
-        // get current price
-        (uint160 sqrtPriceX96, , , , , , ) = params.pool.slot0();
-        // get the equivalent amount of liquidity from amount0 & amount1 in current price
-        response.liquidity = LiquidityAmounts.getLiquidityForAmounts(
-            sqrtPriceX96,
-            TickMath.getSqrtRatioAtTick(params.tickLower),
-            TickMath.getSqrtRatioAtTick(params.tickUpper),
-            amount0,
-            amount1
-        );
-
-        // call mint()
-        // verify if this liquidity is necessary
-        uint256 addedAmount0;
-        uint256 addedAmount1;
-        if (response.liquidity > 0) {
-            (addedAmount0, addedAmount1) = params.pool.mint(
-                address(this),
-                params.tickLower,
-                params.tickUpper,
-                response.liquidity,
-                // FIXME
-                // depends on what verification we need to check inside callback
-                abi.encode(msg.sender)
-            );
-        }
-
-        // make base & quote into the right order
-        if (isBase0Quote1) {
-            response.base = addedAmount0;
-            response.quote = addedAmount1;
-            response.feeGrowthInsideLastBase = feeGrowthInside0LastX128;
-            response.feeGrowthInsideLastQuote = feeGrowthInside1LastX128;
-        } else {
-            response.quote = addedAmount0;
-            response.base = addedAmount1;
-            response.feeGrowthInsideLastQuote = feeGrowthInside0LastX128;
-            response.feeGrowthInsideLastBase = feeGrowthInside1LastX128;
-        }
+    function swap(
+        address _pool,
+        bool _baseToQuote,
+        int256 _amount,
+        uint96 sqrtPriceLimitX96
+    ) internal returns (uint256 base, uint256 quote) {
+        revert();
     }
 
-    function _isBase0Quote1(
-        IUniswapV3Pool pool,
-        address baseToken,
-        address quoteToken
-    ) private view returns (bool) {
-        address token0 = pool.token0();
-        address token1 = pool.token1();
-        if (baseToken == token0 && quoteToken == token1) return true;
-        if (baseToken == token1 && quoteToken == token0) return false;
-        // pool token mismatched. should throw from earlier check
-        revert("UB_PTM");
+    function collect(
+        address _pool,
+        int256 _tickLower,
+        int256 _tickUpper
+    ) internal returns (uint256 feeBase, uint256 feeQuote) {
+        revert();
     }
 
-    function _getPositionLiquidity(
-        IUniswapV3Pool pool,
-        int24 tickLower,
-        int24 tickUpper
-    ) private view returns (uint128 liquidity) {
-        bytes32 positionKey = PositionKey.compute(address(this), tickLower, tickUpper);
-        (liquidity, , , , ) = pool.positions(positionKey);
+    // view functions
+    function getAmountsForLiquidity(
+        address _pool,
+        uint24 _tickLower,
+        uint24 _tickUpper,
+        uint256 _liquidty
+    ) internal view returns (uint256 base, uint256 quote) {
+        revert();
+    }
+
+    function getQuoteToBase(
+        address _pool,
+        bool _exactQuote,
+        uint256 _amount
+    ) internal view returns (uint256 base) {
+        revert();
+    }
+
+    function getBaseToQuote(
+        address _pool,
+        bool _exactBase,
+        uint256 _amount
+    ) internal view returns (uint256 quote) {
+        revert();
+    }
+
+    function isExistedPool(address factory, address pool) internal view returns (bool) {
+        return IUniswapV3Pool(pool).factory() == factory;
     }
 }
