a8bfe8bd7fb93c723f4c03a69b04900c93f03514
Dylan Yang <dogi9527dogi9527@gmail.com>
2021-06-09 16:09:49 +0800

    chore: rebase main branch

--- contracts/ClearingHouse.sol
+++ contracts/ClearingHouse.sol
@@ -1,4 +1,5 @@
-pragma solidity 0.8.4;
+pragma solidity 0.7.6;
+
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 import { IUniswapV3Factory } from "@uniswap/v3-core/contracts/interfaces/IUniswapV3Factory.sol";
