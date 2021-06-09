d71d7ef4f1258b7c065e63ae68cf6fa3c07a5176
Vinta Chen <vinta.chen@gmail.com>
2021-06-09 14:19:28 +0800

    test both above/under price

--- test/integrationTests/UniswapV3Broker.test.ts
+++ test/integrationTests/UniswapV3Broker.test.ts
@@ -16,7 +16,6 @@ describe("UniswapV3Broker", () => {
         pool = _pool
         base = _base
         quote = _quote
-        await pool.initialize(encodePriceSqrt(1, 10))
 
         const UniswapV3BrokerFactory = await ethers.getContractFactory("TestUniswapV3Broker")
         uniswapV3Broker = (await UniswapV3BrokerFactory.deploy(factory.address)) as TestUniswapV3Broker
@@ -26,7 +25,9 @@ describe("UniswapV3Broker", () => {
     })
 
     describe("#mint", () => {
-        it("mint", async () => {
+        it("mint under price", async () => {
+            await pool.initialize(encodePriceSqrt(1, 10))
+
             await expect(
                 uniswapV3Broker.mint({
                     pool: pool.address,
@@ -42,10 +43,36 @@ describe("UniswapV3Broker", () => {
                 .withArgs(
                     uniswapV3Broker.address,
                     uniswapV3Broker.address,
-                    "50000",
-                    "50200",
-                    parseEther("1"),
+                    50000,
+                    50200,
+                    "999999999994411796", // around 1
                     parseEther("0.000816820841"),
+                    parseEther("0"),
+                )
+        })
+
+        it("mint above price", async () => {
+            await pool.initialize(encodePriceSqrt(200, 1))
+
+            await expect(
+                uniswapV3Broker.mint({
+                    pool: pool.address,
+                    baseToken: base.address,
+                    quoteToken: quote.address,
+                    tickLower: "50000",
+                    tickUpper: "50200",
+                    baseAmount: parseEther("0"),
+                    quoteAmount: parseEther("0.122414646"),
+                }),
+            )
+                .to.emit(pool, "Mint")
+                .withArgs(
+                    uniswapV3Broker.address,
+                    uniswapV3Broker.address,
+                    50000,
+                    50200,
+                    "1000000000109464931", // around 1
+                    parseEther("0"),
                     parseEther("0.122414646"),
                 )
         })
