28156a2a815b26f2f1e1f3f84185ae10ce59d719
shao <wraecca@gmail.com>
2021-06-10 15:57:25 +0800

    fix mint test case

--- test/uniswapV3/UniswapV3Broker.test.ts
+++ test/uniswapV3/UniswapV3Broker.test.ts
@@ -3,7 +3,7 @@ import { expect } from "chai"
 import { ethers, waffle } from "hardhat"
 import { TestERC20, TestUniswapV3Broker, UniswapV3Pool } from "../../typechain"
 import { poolFixture } from "../shared/fixtures"
-import { encodePriceSqrt } from "../shared/utilities"
+import { BaseQuoteAmountPair, encodePriceSqrt, Token01AmountPair, token01toBaseQuote } from "../shared/utilities"
 
 describe("UniswapV3Broker", () => {
     let pool: UniswapV3Pool
@@ -27,18 +27,32 @@ describe("UniswapV3Broker", () => {
 
     // https://docs.google.com/spreadsheets/d/1H8Sn0YHwbnEjhhA03QOVfOFPPFZUX5Uasg14UY9Gszc/edit#gid=1867451918
     describe("# mint", () => {
+        let token01Amount: Token01AmountPair
+        let baseQuoteAmount: BaseQuoteAmountPair
+
+        beforeEach(() => {
+            token01Amount = { token0Amount: parseEther("0.000816820841"), token1Amount: parseEther("0.122414646") }
+            baseQuoteAmount = token01toBaseQuote(base.address, quote.address, token01Amount)
+        })
+
         it("mint range order above current price", async () => {
             await pool.initialize(encodePriceSqrt(1, 1))
+
+            // when above price, token1 = 0
+            token01Amount.token1Amount = 0
+            baseQuoteAmount = token01toBaseQuote(base.address, quote.address, token01Amount)
+            const { baseAmount, quoteAmount } = baseQuoteAmount
+            const { token0Amount, token1Amount } = token01Amount
 
             await expect(
                 uniswapV3Broker.mint({
                     pool: pool.address,
                     baseToken: base.address,
                     quoteToken: quote.address,
-                    tickLower: "50000",
-                    tickUpper: "50200",
-                    baseAmount: parseEther("0.000816820841"),
-                    quoteAmount: parseEther("0.122414646"),
+                    tickLower: 50000,
+                    tickUpper: 50200,
+                    baseAmount,
+                    quoteAmount,
                 }),
             )
                 .to.emit(pool, "Mint")
@@ -48,13 +62,19 @@ describe("UniswapV3Broker", () => {
                     50000,
                     50200,
                     "999999999994411796", // around 1
-                    parseEther("0.000816820841"),
-                    parseEther("0"),
+                    token0Amount,
+                    token1Amount,
                 )
         })
 
         it("mint range order under current price", async () => {
             await pool.initialize(encodePriceSqrt(200, 1))
+
+            // when under price, token0 = 0
+            token01Amount.token0Amount = 0
+            baseQuoteAmount = token01toBaseQuote(base.address, quote.address, token01Amount)
+            const { baseAmount, quoteAmount } = baseQuoteAmount
+            const { token0Amount, token1Amount } = token01Amount
 
             await expect(
                 uniswapV3Broker.mint({
@@ -63,8 +83,8 @@ describe("UniswapV3Broker", () => {
                     quoteToken: quote.address,
                     tickLower: "50000",
                     tickUpper: "50200",
-                    baseAmount: parseEther("0"),
-                    quoteAmount: parseEther("0.122414646"),
+                    baseAmount,
+                    quoteAmount,
                 }),
             )
                 .to.emit(pool, "Mint")
@@ -74,8 +94,8 @@ describe("UniswapV3Broker", () => {
                     50000,
                     50200,
                     "1000000000109464931", // around 1
-                    parseEther("0"),
-                    parseEther("0.122414646"),
+                    token0Amount,
+                    token1Amount,
                 )
         })
     })
