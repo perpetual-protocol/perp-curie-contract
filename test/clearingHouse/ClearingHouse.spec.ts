66584138200fab210b994bf467184e1d99517688
shao <wraecca@gmail.com>
2021-06-23 21:20:24 +0800

    update ch spec, ensure base=token0 quote=token1 when ch.addPool

--- test/clearingHouse/ClearingHouse.spec.ts
+++ test/clearingHouse/ClearingHouse.spec.ts
@@ -3,7 +3,7 @@ import { BigNumber } from "@ethersproject/bignumber"
 import { expect } from "chai"
 import { waffle } from "hardhat"
 import { ClearingHouse } from "../../typechain"
-import { deployERC20, mockedClearingHouseFixture } from "./fixtures"
+import { LONGER_THAN, mockedClearingHouseFixture, mockedTokenTo, SHORTER_THAN } from "./fixtures"
 
 describe("ClearingHouse Spec", () => {
     const [wallet] = waffle.provider.getWallets()
@@ -11,17 +11,18 @@ describe("ClearingHouse Spec", () => {
     const EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000"
     const POOL_A_ADDRESS = "0x000000000000000000000000000000000000000A"
     const POOL_B_ADDRESS = "0x000000000000000000000000000000000000000b"
-    const POOL_C_ADDRESS = "0x000000000000000000000000000000000000000C"
     const DEFAULT_FEE = 3000
 
     let clearingHouse: ClearingHouse
     let baseToken: MockContract
+    let quoteToken: MockContract
     let uniV3Factory: MockContract
 
     beforeEach(async () => {
         const _clearingHouseFixture = await loadFixture(mockedClearingHouseFixture)
         clearingHouse = _clearingHouseFixture.clearingHouse
         baseToken = _clearingHouseFixture.mockedBaseToken
+        quoteToken = _clearingHouseFixture.mockedVUSDC
         uniV3Factory = _clearingHouseFixture.mockedUniV3Factory
 
         // uniV3Factory.getPool always returns POOL_A_ADDRESS
@@ -43,12 +44,12 @@ describe("ClearingHouse Spec", () => {
         })
 
         it("add multiple UniswapV3 pools", async () => {
-            const baseToken2 = await deployERC20()
+            const baseToken2 = await mockedTokenTo(SHORTER_THAN, quoteToken.address)
             await clearingHouse.addPool(baseToken.address, DEFAULT_FEE)
 
             // mock the return address of `getPool`
-            uniV3Factory.smocked.getPool.will.return.with((token0: string, token1: string, feeRatio: BigNumber) => {
-                return POOL_C_ADDRESS
+            uniV3Factory.smocked.getPool.will.return.with(() => {
+                return POOL_B_ADDRESS
             })
             await clearingHouse.addPool(baseToken2.address, DEFAULT_FEE)
 
@@ -56,11 +57,11 @@ describe("ClearingHouse Spec", () => {
             const pool = await clearingHouse.getPool(baseToken.address)
             expect(pool).to.eq(POOL_A_ADDRESS)
             const pool2 = await clearingHouse.getPool(baseToken2.address)
-            expect(pool2).to.eq(POOL_C_ADDRESS)
+            expect(pool2).to.eq(POOL_B_ADDRESS)
         })
 
         it("force error, pool is not existent in uniswap v3", async () => {
-            uniV3Factory.smocked.getPool.will.return.with((token0: string, token1: string, feeRatio: BigNumber) => {
+            uniV3Factory.smocked.getPool.will.return.with(() => {
                 return EMPTY_ADDRESS
             })
             await expect(clearingHouse.addPool(baseToken.address, DEFAULT_FEE)).to.be.revertedWith("CH_NEP")
@@ -69,6 +70,11 @@ describe("ClearingHouse Spec", () => {
         it("force error, pool is existent in ClearingHouse", async () => {
             await clearingHouse.addPool(baseToken.address, DEFAULT_FEE)
             await expect(clearingHouse.addPool(baseToken.address, DEFAULT_FEE)).to.be.revertedWith("CH_EP")
+        })
+
+        it("force error, base must be smaller than quote to force base = token0 and quote = token1", async () => {
+            const tokenWithLongerAddr = await mockedTokenTo(LONGER_THAN, quoteToken.address)
+            await expect(clearingHouse.addPool(tokenWithLongerAddr.address, DEFAULT_FEE)).to.be.revertedWith("CH_IB")
         })
     })
 })
