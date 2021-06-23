1988fcdf2dac4de82562ce55cdf784173121c0f7
shao <wraecca@gmail.com>
2021-06-23 12:41:44 +0800

    use getSigners instead of hard codec

--- scripts/01-deploy-USDC.ts
+++ scripts/01-deploy-USDC.ts
@@ -1,13 +1,12 @@
 import { ethers } from "hardhat"
 
 async function main() {
-    const testnetDeployAddr = "0x9e9dfaccabeecda6dd913b3685c9fe908f28f58c"
-
     const tokenFactory = await ethers.getContractFactory("TestERC20")
     const usdc = await tokenFactory.deploy("TestUSDC", "TestUSDC")
     console.log("USDC deployed to:", usdc.address)
 
-    await usdc.setMinter(testnetDeployAddr)
+    const deployer = ethers.getSigners[0]
+    await usdc.setMinter(deployer.address)
 }
 
 main()
