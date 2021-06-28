d0edc428a3dd56f43fc85c53a2ce4ddec42c0c0d
Vinta Chen <vinta.chen@gmail.com>
2021-06-28 18:15:25 +0800

    add UniswapV3Broker.removeLiquidity.test

---
+++ test/uniswapV3/UniswapV3Broker.removeLiquidity.test.ts
@@ -0,0 +1,208 @@
+import { expect } from "chai"
+import { parseEther } from "ethers/lib/utils"
+import { ethers, waffle } from "hardhat"
+import { TestERC20, TestUniswapV3Broker, UniswapV3Pool } from "../../typechain"
+import { base0Quote1PoolFixture } from "../shared/fixtures"
+import { encodePriceSqrt } from "../shared/utilities"
+
+describe("UniswapV3Broker.removeLiquidity", () => {
+    const [wallet] = waffle.provider.getWallets()
+    const loadFixture: ReturnType<typeof waffle.createFixtureLoader> = waffle.createFixtureLoader([wallet])
+    let pool: UniswapV3Pool
+    let baseToken: TestERC20
+    let quoteToken: TestERC20
+    let uniswapV3Broker: TestUniswapV3Broker
+
+    // https://docs.google.com/spreadsheets/d/1H8Sn0YHwbnEjhhA03QOVfOFPPFZUX5Uasg14UY9Gszc/edit#gid=1867451918
+    beforeEach(async () => {
+        const {
+            factory,
+            pool: _pool,
+            baseToken: _baseToken,
+            quoteToken: _quoteToken,
+        } = await loadFixture(base0Quote1PoolFixture)
+        pool = _pool
+        baseToken = _baseToken
+        quoteToken = _quoteToken
+
+        const UniswapV3BrokerFactory = await ethers.getContractFactory("TestUniswapV3Broker")
+        uniswapV3Broker = (await UniswapV3BrokerFactory.deploy(factory.address)) as TestUniswapV3Broker
+
+        // broker has the only permission to mint vToken
+        await baseToken.setMinter(uniswapV3Broker.address)
+        await quoteToken.setMinter(uniswapV3Broker.address)
+    })
+
+    // https://docs.google.com/spreadsheets/d/1xcWBBcQYwWuWRdlHtNv64tOjrBCnnvj_t1WEJaQv8EY/edit#gid=150902425
+    it("burn and get 100% quote token", async () => {
+        await pool.initialize(encodePriceSqrt(200, 1))
+        const base = "0"
+        const quote = parseEther("0.122414646")
+        await uniswapV3Broker.addLiquidity({
+            pool: pool.address,
+            baseToken: baseToken.address,
+            quoteToken: quoteToken.address,
+            lowerTick: "50000",
+            upperTick: "50200",
+            base,
+            quote,
+        })
+
+        await expect(
+            uniswapV3Broker.removeLiquidity({
+                pool: pool.address,
+                lowerTick: "50000",
+                upperTick: "50200",
+                liquidity: "1000000000109464931",
+            }),
+        )
+            .to.emit(pool, "Burn")
+            .withArgs(
+                uniswapV3Broker.address,
+                50000,
+                50200,
+                "1000000000109464931", // around 1
+                base,
+                "122414645999999999", // rounding error, it's around 0.122414646
+            )
+    })
+
+    it("burn and get 50% quote token", async () => {
+        await pool.initialize(encodePriceSqrt(200, 1))
+        const base = "0"
+        const quote = parseEther("0.122414646")
+
+        await uniswapV3Broker.addLiquidity({
+            pool: pool.address,
+            baseToken: baseToken.address,
+            quoteToken: quoteToken.address,
+            lowerTick: "50000",
+            upperTick: "50200",
+            base,
+            quote,
+        })
+
+        await expect(
+            uniswapV3Broker.removeLiquidity({
+                pool: pool.address,
+                lowerTick: "50000",
+                upperTick: "50200",
+                liquidity: "500000000054732465",
+            }),
+        )
+            .to.emit(pool, "Burn")
+            .withArgs(
+                uniswapV3Broker.address,
+                50000,
+                50200,
+                "500000000054732465", // around 0.5
+                base,
+                "61207322999999999", // quote.div(2)
+            )
+    })
+
+    // TODO: add partial burn cases for all the following cases in the future
+    it("burn and get 100% base token", async () => {
+        await pool.initialize(encodePriceSqrt(1, 1))
+        const base = parseEther("0.000816820841")
+        const quote = "0"
+
+        await uniswapV3Broker.addLiquidity({
+            pool: pool.address,
+            baseToken: baseToken.address,
+            quoteToken: quoteToken.address,
+            lowerTick: 50000,
+            upperTick: 50200,
+            base,
+            quote,
+        })
+
+        await expect(
+            uniswapV3Broker.removeLiquidity({
+                pool: pool.address,
+                lowerTick: "50000",
+                upperTick: "50200",
+                liquidity: "999999999994411796",
+            }),
+        )
+            .to.emit(pool, "Burn")
+            .withArgs(
+                uniswapV3Broker.address,
+                50000,
+                50200,
+                "999999999994411796", // around 1
+                "816820840999999", // rounding error, around 0.000816820841
+                "0",
+            )
+    })
+
+    it("burn and get 100% quote and base token", async () => {
+        await pool.initialize(encodePriceSqrt(151.3733069, 1))
+
+        await uniswapV3Broker.addLiquidity({
+            pool: pool.address,
+            baseToken: baseToken.address,
+            quoteToken: quoteToken.address,
+            lowerTick: 50000, // 148.3760629
+            upperTick: 50400, // 154.4310961
+            base: parseEther("0.000808693720084599"),
+            quote: parseEther("0.122414646"),
+        })
+
+        await expect(
+            uniswapV3Broker.removeLiquidity({
+                pool: pool.address,
+                lowerTick: "50000",
+                upperTick: "50400",
+                liquidity: "999999986406400213",
+            }),
+        )
+            .to.emit(pool, "Burn")
+            .withArgs(
+                uniswapV3Broker.address,
+                50000,
+                50400,
+                "999999986406400213", // around 1
+                "808693720084598", // rounding error, around 0.000808693720084599
+                "122414645999999999", // rounding error, around 0.122414646
+            )
+    })
+
+    it("burn zero liquidity when no liquidity", async () => {
+        await expect(
+            uniswapV3Broker.removeLiquidity({
+                pool: pool.address,
+                lowerTick: 50000, // 148.3760629
+                upperTick: 50400, // 154.4310961
+                liquidity: parseEther("1"),
+            }),
+        ).to.be.reverted
+    })
+
+    it("burn zero liquidity when there was only quote liquidity", async () => {
+        await pool.initialize(encodePriceSqrt(200, 1))
+        const base = "0"
+        const quote = parseEther("0.122414646")
+
+        await uniswapV3Broker.addLiquidity({
+            pool: pool.address,
+            baseToken: baseToken.address,
+            quoteToken: quoteToken.address,
+            lowerTick: "50000",
+            upperTick: "50200",
+            base,
+            quote,
+        })
+
+        await expect(
+            uniswapV3Broker.removeLiquidity({
+                pool: pool.address,
+                lowerTick: "50000",
+                upperTick: "50200",
+                liquidity: parseEther("0"),
+            }),
+        )
+            .to.emit(pool, "Burn")
+            .withArgs(uniswapV3Broker.address, 50000, 50200, "0", "0", "0")
+    })
+})
