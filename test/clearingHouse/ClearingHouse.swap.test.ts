3159885c94b1b3ee03bd64ad3baa338f2361d851
Vinta Chen <vinta.chen@gmail.com>
2021-06-30 17:23:36 +0800

    remove misplaced test case from CH.swap.test

--- test/clearingHouse/ClearingHouse.swap.test.ts
+++ test/clearingHouse/ClearingHouse.swap.test.ts
@@ -70,51 +70,5 @@ describe("ClearingHouse.swap", () => {
             const { available: bobQuoteAvailable } = await clearingHouse.getTokenInfo(bob.address, quoteToken.address)
             expect(bobQuoteAvailable.gt(toWei(0))).to.be.true
         })
-
-        it("# burn quote 10 when debt = 10, available < 10", async () => {
-            await pool.initialize(encodePriceSqrt("154.4310961", "1"))
-
-            await clearingHouse.connect(alice).addLiquidity({
-                baseToken: baseToken.address,
-                base: toWei(0),
-                quote: toWei(10),
-                lowerTick: 50200,
-                upperTick: 50400,
-            })
-
-            await collateral.mint(bob.address, toWei(100))
-            await collateral.connect(bob).approve(clearingHouse.address, toWei(100))
-            await clearingHouse.connect(bob).deposit(toWei(100))
-            await clearingHouse.connect(bob).mint(baseToken.address, toWei(1))
-            await clearingHouse.connect(bob).mint(quoteToken.address, toWei(100))
-
-            await clearingHouse.connect(bob).swap({
-                // sell base
-                baseToken: baseToken.address,
-                quoteToken: quoteToken.address,
-                isBaseToQuote: true,
-                isExactInput: true,
-                amount: toWei(0.001),
-                sqrtPriceLimitX96: 0,
-            })
-
-            const { available: aliceQuoteAvailable } = await clearingHouse.getTokenInfo(
-                alice.address,
-                quoteToken.address,
-            )
-            expect(aliceQuoteAvailable.lt(toWei(10))).to.be.true
-            await expect(clearingHouse.connect(alice).burn(quoteToken.address, aliceQuoteAvailable))
-                .to.emit(clearingHouse, "Burned")
-                .withArgs(quoteToken.address, aliceQuoteAvailable)
-
-            expect(await clearingHouse.getTokenInfo(alice.address, quoteToken.address)).to.deep.eq([
-                toWei(0), // available
-                toWei(0), // debt
-                toWei(0), // fee FIXME alice should has fee
-            ])
-
-            // const profit = available.sub(previousAvailable)
-            // expect(await clearingHouse.getFreeCollateral(alice.address)).to.eq(toWei(10).add(profit))
-        })
     })
 })
