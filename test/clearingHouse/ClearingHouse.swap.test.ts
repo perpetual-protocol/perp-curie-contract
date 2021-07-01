685946fd4ba9deb94677db65046cb76de65052ed
tienshaoku <tienshaoku@gmail.com>
2021-07-01 16:34:51 +0800

    # Failed test fixed.

--- test/clearingHouse/ClearingHouse.swap.test.ts
+++ test/clearingHouse/ClearingHouse.swap.test.ts
@@ -66,7 +66,6 @@ describe("ClearingHouse.swap", () => {
             expect(await clearingHouse.getTokenInfo(bob.address, baseToken.address)).to.deep.eq([
                 toWei(1 - 0.01), // available
                 toWei(1), // debt
-                toWei(0),
             ])
             const { available: bobQuoteAvailable } = await clearingHouse.getTokenInfo(bob.address, quoteToken.address)
             expect(bobQuoteAvailable.gt(toWei(0))).to.be.true
