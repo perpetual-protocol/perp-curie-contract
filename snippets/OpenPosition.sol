736889bb82ea15b5b092a064fbca18906ed10e8b
shao <wraecca@gmail.com>
2021-07-02 13:06:15 +0800

    fix settle

--- snippets/OpenPosition.sol
+++ snippets/OpenPosition.sol
@@ -119,14 +119,6 @@ contract OpenPosition {
             return;
         }
 
-        uint256 burnableQuote = max(quoteAvailable, quoteDebt);
-        burn(quote, burnableQuote);
-        if (quoteDebt > 0) {
-            // under collateral, insurance fund get hurt
-            _increaseBadDebt(quoteDebt);
-        } else {
-            account[positionOwner].collateral += quoteAvailable;
-        }
-        _clearQuoteBalanceAndDebt();
+        burn(quote, max(quoteAvailable, quoteDebt));
     }
 }
