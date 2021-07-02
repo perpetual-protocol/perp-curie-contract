4705355d95f5f404d5d60be9638b3f05bc920e1a
shao <wraecca@gmail.com>
2021-07-02 11:34:20 +0800

    change spec of open and close position

--- snippets/OpenPosition.sol
+++ snippets/OpenPosition.sol
@@ -1,51 +1,71 @@
+/**
+- open position: 
+  - even if the caller is an maker, it wont affect her open order
+  - there're 4 combination (isBaseToQuote * isExactInput)
+  - it settles funding
+  - it mints missing amount of vToken for swapping
+  - it does not mint anything if it's already sufficient
+- close position:
+  - [ ] if taker has exposure in the pool
+    1. liquidity, base available and base debt will be zero 0 ( but i feel it doesnt like how exchange work )
+    2. positionSize (net) is 0, liquidity might change but not necessary to be removed ( no idea how to impl ) 
+  - taker has ETH position
+    - when loss
+        - if the free collateral is not enough to cover the loss, revert (TODO: insurance fund)
+        - if the collateral is enough to cover the loss, free collateral will be less
+    - when profit
+        - free collateral increase
+  - taker has ETH and BTC position 
+    - when loss
+        - if the free collateral is not enough to cover the loss, then BTC will be able to be liquidated
+ */
+
 contract OpenPosition {
-    // compatible with v1 interface, this does not consider maker.
-    // TODO: can we have better interface
+    // does not consider maker
     function openPosition(
-        baseAddr, // pool
-        isBaseToQuote, // side
-        margin,
-        leverage,
-        baseLimit
+        baseToken, // pool
+        isBaseToQuote,
+        isExactInput,
+        amount,
+        sqrtPriceLimitX96
     ) {
         _settleFunding();
 
-        uint256 base = uni.getExactBaseFromQuote(margin * leverage);
-        uint256 sqrtPriceLimit = getSqrtPriceLimit(isBaseToQuote, baseLimit);
-        if (isIncreasePos) {
-            deposit(margin);
+        // calculate if we need to mint more quote or base to buy the exact output
+        if (!isExactInput) {
             if (isBaseToQuote) {
-                // increase short
-                mint(baseToken, base);
-                swap(baseToken, quoteToken, baseToQuoteTrue, exactInputTrue, base, sqrtPriceLimit);
+                // taker want to get exact quote from base
+                // calc how many base is needed for the exact quote
+                uint256 requiredBase = uni.getExactBaseFromQuote(amount);
+                // make sure taker has enough base to buy the desired quote
+                if (getBaseAvailable < requiredBase) {
+                    mint(baseToken, insufficientBase);
+                }
             } else {
-                // increase long
-                mint(quoteToken, margin * leverage);
-                swap(baseToken, quoteToken, baseToQuoteFalse, exactInputTrue, margin * leverage, sqrtPriceLimit);
-            }
-        } else {
-            // openReversePosition
-            if (isReducePosition()) {
-                swap(baseToken, quoteToken, baseToQuoteTrue, exactInputTrue, base, sqrtPriceLimit);
-                burn(baseToken, getBaseAvailable(msgSender()));
-                burn(quoteToken, getQuoteAvailable(msgSender()));
-                _settle(msgSender());
-            } else {
-                // close and open revers position
-                _closePosition(pool, msgSender(), sqrtPriceLimit);
-                increasePosition(); // check the logic above
+                // taker want to get exact base from quote
+                // calc how many quote is needed for the exact base
+                uint256 requiredQuote = uni.getExactQuoteFromBase(amount);
+                // make sure taker has enough quote to buy the desired base
+                if (getQuoteAvailable < requiredQuote) {
+                    mint(quoteToken, insufficientQuote);
+                }
             }
         }
 
-        if (!isPositionClosed()) {
-            checkInitMarginRequirement();
+        swap(baseToken, quoteToken, isBaseToQuote, isExactInput, amount, sqrtPriceLimitX96);
+        if (isPositionClosed()) {
+            _settle(msgSender());
         }
     }
 
-    function closePosition(baseToken, slippageProtection) {
-        checkMarginRequirement();
-        _closePosition(getPool(baseToken), msgSender(), getSqrtPriceLimit(slippageProtection));
-        _settle(msgSender());
+    function closePosition(baseToken, sqrtPriceLimit) {
+        _removeOrders(baseToken);
+        _closePosition(getPool(baseToken), msgSender(), sqrtPriceLimit);
+    }
+
+    function liquidate(posOwner, baseToken) {
+        checkLiquidationRequirement(posOwner, baseToken);
+        _closePosition(getPool(baseToken), posOwner, sqrtPriceLimit);
     }
 
     function _closePosition(
@@ -53,6 +73,8 @@ contract OpenPosition {
         positionOwner,
         sqrtPriceLimit
     ) {
+        _settleFunding();
+
         Position oldPosition = getPosition(positionOwner);
         int256 basePnl = getBaseAvailable(positionOwner) - getBaseDebt(positionOwner);
         // if has loss
@@ -70,31 +92,40 @@ contract OpenPosition {
             swap(baseToken, quoteToken, baseToQuoteTrue, exactInputTrue, base, sqrtPriceLimit);
         }
         burn(baseToken, baseDebt);
-        // expect baseDebt is cleared
+        // expect baseAvailable and baseDebt are cleared
+
+        _settle(posOwner);
     }
 
-    // settle pnl to trader's collateral
+    function mint(token, amount) {
+        _mint(token, amount, ForceIsFalse);
+    }
+
+    function _mint(
+        token,
+        amount,
+        isForce
+    ) {
+        // TODO move mint's code here
+        // *** PASTED *** //
+        if (!isForce) {
+            checkInitMarginRequirement();
+        }
+    }
+
+    // settle pnl to trader's collateral when there's no position is hold
     function _settle(positionOwner) {
-        Position oldPosition = getPosition(positionOwner);
-        int256 quotePnl = getQuote(positionOwner) - getQuoteDebt(positionOwner);
-        if (quotePnl < 0) {
-            if (getFreeCollateral(positionOwner) < quotePnl) {
-                // start to liquidate other market because the collateral is not enough
-                // TODO: how two decide slippage protection when closing all other markets
-                for (uint256 i = pools.length; i > 0; i--) {
-                    _closePosition(pool, positionOwner, noSlippageProtection);
-                    // TODO: improve algo for gas
-                    if (getFreeCollateral(positionOwner) >= quotePnl) {
-                        break;
-                    }
-                }
-            }
-            burn(quoteToken, quoteDebt, oldPosition[quoteToken].available);
-            if (quotePnlAfter < 0) {
-                _increaseBadDebt(quotePnlAfter);
-            }
+        if (!_hasZeroPosition()) {
+            return;
+        }
+
+        uint256 burnableQuote = max(quoteAvailable, quoteDebt);
+        burn(quote, burnableQuote);
+        if (quoteDebt > 0) {
+            // under collateral, insurance fund get hurt
+            _increaseBadDebt(quoteDebt);
         } else {
-            account[positionOwner].collateral += quotePnl;
+            account[positionOwner].collateral += quoteAvailable;
         }
         _clearQuoteBalanceAndDebt();
     }
