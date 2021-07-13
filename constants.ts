aeb65661e5cc23e3e131e7681667cc61b6175bd0
shao <wraecca@gmail.com>
2021-07-13 15:21:55 +0800

    fix typo

--- constants.ts
+++ constants.ts
@@ -1,8 +1,11 @@
 import _ from "lodash"
 export const RINKEBY_DEPLOYER_MNEMONIC = _.defaultTo(`${process.env["RINKEBY_DEPLOYER_MNEMONIC"]}`, "")
 export const RINKEBY_WEB3_ENDPOINT = _.defaultTo(`${process.env["RINKEBY_WEB3_ENDPOINT"]}`, "")
-export const ARBITRUM_RINKEBY_DEPLOYER_MNEMONIC = _.defaultTo(`${process.env["RINKEBY_DEPLOYER_MNEMONIC"]}`, "")
-export const ARBITRUM_RINKEBY_WEB3_ENDPOINT = _.defaultTo(`${process.env["RINKEBY_WEB3_ENDPOINT"]}`, "")
+export const ARBITRUM_RINKEBY_DEPLOYER_MNEMONIC = _.defaultTo(
+    `${process.env["ARBITRUM_RINKEBY_DEPLOYER_MNEMONIC"]}`,
+    "",
+)
+export const ARBITRUM_RINKEBY_WEB3_ENDPOINT = _.defaultTo(`${process.env["ARBITRUM_RINKEBY_WEB3_ENDPOINT"]}`, "")
 
 if (_.isEmpty(RINKEBY_DEPLOYER_MNEMONIC)) {
     console.warn("RINKEBY_MNEMONIC is empty")
