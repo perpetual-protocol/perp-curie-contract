69943d40fce6be7bb367523ca30b2e805fa3381f
shao <wraecca@gmail.com>
2021-07-30 17:09:56 +0800

    fix updated path

--- contracts/base/Validation.sol
+++ contracts/base/Validation.sol
@@ -1,6 +1,6 @@
 pragma solidity 0.7.6;
 
-import { ArbBlockContext } from "../util/ArbBlockContext.sol";
+import { ArbBlockContext } from "../arbitrum/ArbBlockContext.sol";
 
 abstract contract Validation is ArbBlockContext {
     modifier checkDeadline(uint256 deadline) {
