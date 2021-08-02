3c79e5860e6ed0364a103ba7b68d63fe20e4dcc3
tienshaoku <tienshaoku@gmail.com>
2021-08-03 01:00:06 +0800

    fromWei() replaced with formatEther().

--- test/helper/number.ts
+++ test/helper/number.ts
@@ -1,10 +1,2 @@
-import bn from "bignumber.js"
-import { BigNumberish } from "ethers"
-import { formatEther } from "ethers/lib/utils"
-
-export function fromWei(val: BigNumberish, decimals = 18): bn {
-    return new bn(formatEther(val))
-}
-
 export const getMinTick = (tickSpacing: number) => Math.ceil(-887272 / tickSpacing) * tickSpacing
 export const getMaxTick = (tickSpacing: number) => Math.floor(887272 / tickSpacing) * tickSpacing
