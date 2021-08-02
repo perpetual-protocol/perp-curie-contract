6037de534a6cfaf950a42d9ff2646b9bb42601bf
shao <wraecca@gmail.com>
2021-08-03 05:47:26 +0800

    increase long position (Q2B)

--- test/clearingHouse/ClearingHouse.swap.test.ts
+++ test/clearingHouse/ClearingHouse.swap.test.ts
@@ -2,13 +2,13 @@ import { expect } from "chai"
 import { parseEther, parseUnits } from "ethers/lib/utils"
 import { ethers, waffle } from "hardhat"
 import { ClearingHouse, TestERC20, UniswapV3Pool, Vault, VirtualToken } from "../../typechain"
+import { getMaxTick, getMinTick } from "../helper/number"
 import { deposit } from "../helper/token"
 import { encodePriceSqrt } from "../shared/utilities"
 import { BaseQuoteOrdering, createClearingHouseFixture } from "./fixtures"
 
 describe("ClearingHouse.swap", () => {
-    const EMPTY_ADDRESS = "0x0000000000000000000000000000000000000000"
-    const [admin, alice, bob] = waffle.provider.getWallets()
+    const [admin, alice, bob, carol] = waffle.provider.getWallets()
     const loadFixture: ReturnType<typeof waffle.createFixtureLoader> = waffle.createFixtureLoader([admin])
     let clearingHouse: ClearingHouse
     let vault: Vault
@@ -17,6 +17,8 @@ describe("ClearingHouse.swap", () => {
     let quoteToken: VirtualToken
     let pool: UniswapV3Pool
     let collateralDecimals: number
+    let lowerTick: number
+    let upperTick: number
 
     beforeEach(async () => {
         const _clearingHouseFixture = await loadFixture(createClearingHouseFixture(BaseQuoteOrdering.BASE_0_QUOTE_1))
@@ -27,51 +29,56 @@ describe("ClearingHouse.swap", () => {
         quoteToken = _clearingHouseFixture.quoteToken
         pool = _clearingHouseFixture.pool
         collateralDecimals = await collateral.decimals()
-
         await clearingHouse.addPool(baseToken.address, "10000")
+
+        const tickSpacing = await pool.tickSpacing()
+        lowerTick = getMinTick(tickSpacing)
+        upperTick = getMaxTick(tickSpacing)
     })
 
     beforeEach(async () => {
+        // prepare maker alice
         await collateral.mint(alice.address, parseUnits("1000", collateralDecimals))
         await deposit(alice, vault, 1000, collateral)
+        await clearingHouse.connect(alice).mint(baseToken.address, parseEther("100"))
         await clearingHouse.connect(alice).mint(quoteToken.address, parseEther("1000"))
+        await pool.initialize(encodePriceSqrt("10", "1"))
+        await clearingHouse.connect(alice).addLiquidity({
+            baseToken: baseToken.address,
+            base: parseEther("100"),
+            quote: parseEther("1000"),
+            lowerTick,
+            upperTick,
+            minBase: 0,
+            minQuote: 0,
+            deadline: ethers.constants.MaxUint256,
+        })
     })
 
-    describe.only("increase short position (B2Q)", () => {
+    describe("increase short position (B2Q)", () => {
         let bobQuoteAvailableBefore
+        let initOpenNotional
         beforeEach(async () => {
-            await pool.initialize(encodePriceSqrt("154.4310961", "1"))
-
-            await clearingHouse.connect(alice).addLiquidity({
-                baseToken: baseToken.address,
-                base: parseEther("0"),
-                quote: parseEther("1000"),
-                lowerTick: 50200,
-                upperTick: 50400,
-                minBase: 0,
-                minQuote: 0,
-                deadline: ethers.constants.MaxUint256,
-            })
-
             await collateral.mint(bob.address, parseUnits("100", collateralDecimals))
             await deposit(bob, vault, 100, collateral)
 
             await clearingHouse.connect(bob).mint(baseToken.address, parseEther("1"))
             bobQuoteAvailableBefore = (await clearingHouse.getTokenInfo(bob.address, quoteToken.address)).available
             await clearingHouse.connect(bob).swap({
-                // sell base
+                // sell 1 base
                 baseToken: baseToken.address,
                 isBaseToQuote: true,
                 isExactInput: true,
                 amount: parseEther("1"),
                 sqrtPriceLimitX96: 0,
             })
+            initOpenNotional = await clearingHouse.getOpenNotional(bob.address, baseToken.address)
         })
 
         it("openNotional++", async () => {
             const bobQuoteAvailableAfter = (await clearingHouse.getTokenInfo(bob.address, quoteToken.address)).available
             const bobQuoteSpent = bobQuoteAvailableAfter.sub(bobQuoteAvailableBefore)
-            expect(await clearingHouse.getOpenNotional(bob.address, baseToken.address)).to.deep.eq(bobQuoteSpent)
+            expect(initOpenNotional).to.deep.eq(bobQuoteSpent)
         })
 
         it("base available--", async () => {
@@ -83,7 +90,7 @@ describe("ClearingHouse.swap", () => {
 
         it("quote available++", async () => {
             const { available: bobQuoteAvailable } = await clearingHouse.getTokenInfo(bob.address, quoteToken.address)
-            expect(bobQuoteAvailable.gt(parseEther("0"))).to.be.true
+            expect(bobQuoteAvailable.gt(0)).to.be.true
         })
 
         it("realizedPnl remains", async () => {
@@ -91,10 +98,41 @@ describe("ClearingHouse.swap", () => {
             expect(pnl).eq(0)
         })
 
-        describe("reduce 25% position, profit", () => {
-            it("openNotional--")
-            it("realizedPnl++")
-            it("settle realizePnl to collateral (increased)")
+        describe("reduce 25% position (exactInput), profit", () => {
+            beforeEach(async () => {
+                // another trader carol sell base, price down
+                await collateral.mint(carol.address, parseUnits("100", collateralDecimals))
+                await deposit(carol, vault, 100, collateral)
+                await clearingHouse.connect(carol).mint(baseToken.address, parseEther("10"))
+                await clearingHouse.connect(carol).swap({
+                    baseToken: baseToken.address,
+                    isBaseToQuote: true,
+                    isExactInput: true,
+                    amount: parseEther("10"),
+                    sqrtPriceLimitX96: 0,
+                })
+
+                // bob reduce 25% position
+                await clearingHouse.connect(bob).swap({
+                    baseToken: baseToken.address,
+                    isBaseToQuote: false, // quote to base
+                    isExactInput: false, // is exact output (base)
+                    amount: parseEther("0.25"),
+                    sqrtPriceLimitX96: 0,
+                })
+            })
+
+            it("openNotionalAbs--", async () => {
+                const openNotional = await clearingHouse.getOpenNotional(bob.address, baseToken.address)
+                // expect openNotion are same signed
+                expect(openNotional.mul(initOpenNotional).gt(0))
+                expect(openNotional.abs().lt(initOpenNotional.abs())).be.true
+            })
+
+            it("realizedPnl++", async () => {
+                const pnl = await clearingHouse.getOwedRealizedPnl(bob.address)
+                expect(pnl.gt(0)).be.true
+            })
         })
 
         describe("reduce 25% position, loss", () => {
@@ -128,5 +166,87 @@ describe("ClearingHouse.swap", () => {
         })
     })
 
-    describe.skip("increase long position (Q2B)", () => {})
+    describe("increase long position (Q2B)", () => {
+        let bobQuoteAvailableBefore
+        let initOpenNotional
+        beforeEach(async () => {
+            await collateral.mint(bob.address, parseUnits("25", collateralDecimals))
+            await deposit(bob, vault, 25, collateral)
+
+            await clearingHouse.connect(bob).mint(quoteToken.address, parseEther("250"))
+            bobQuoteAvailableBefore = (await clearingHouse.getTokenInfo(bob.address, quoteToken.address)).available
+            await clearingHouse.connect(bob).swap({
+                // buy base
+                baseToken: baseToken.address,
+                isBaseToQuote: false, // quote to base
+                isExactInput: true, // exact quote
+                amount: parseEther("250"),
+                sqrtPriceLimitX96: 0,
+            })
+            initOpenNotional = await clearingHouse.getOpenNotional(bob.address, baseToken.address)
+        })
+
+        it("openNotional++", async () => {
+            const bobQuoteAvailableAfter = (await clearingHouse.getTokenInfo(bob.address, quoteToken.address)).available
+            const bobQuoteSpent = bobQuoteAvailableAfter.sub(bobQuoteAvailableBefore)
+            expect(initOpenNotional).to.deep.eq(bobQuoteSpent)
+        })
+
+        it("base available++", async () => {
+            const baseTokenInfo = await clearingHouse.getTokenInfo(bob.address, baseToken.address)
+            expect(baseTokenInfo.available.gt(0)).be.true
+            expect(baseTokenInfo.debt).to.deep.eq(parseEther("0"))
+        })
+
+        it("quote available--", async () => {
+            const quoteTokenInfo = await clearingHouse.getTokenInfo(bob.address, quoteToken.address)
+            expect(quoteTokenInfo.available).to.deep.eq(parseEther("0"))
+            expect(quoteTokenInfo.debt).to.deep.eq(parseEther("250"))
+        })
+
+        it("realizedPnl remains", async () => {
+            const pnl = await clearingHouse.getOwedRealizedPnl(bob.address)
+            expect(pnl).eq(0)
+        })
+
+        describe("reduce 75% position (exactOutput), loss", () => {
+            beforeEach(async () => {
+                // another trader carol sell base, price down
+                await collateral.mint(carol.address, parseUnits("10000", collateralDecimals))
+                await deposit(carol, vault, 10000, collateral)
+                await clearingHouse.connect(carol).mint(baseToken.address, parseEther("10000"))
+                await clearingHouse.connect(carol).swap({
+                    baseToken: baseToken.address,
+                    isBaseToQuote: true,
+                    isExactInput: false, // exact output (quote)
+                    amount: parseEther("50"),
+                    sqrtPriceLimitX96: 0,
+                })
+
+                const bobPosSize = await clearingHouse.getPositionSize(bob.address, baseToken.address)
+                const partial = bobPosSize.div(4).mul(3)
+                // bob reduce 75% position
+                await clearingHouse.connect(bob).swap({
+                    baseToken: baseToken.address,
+                    isBaseToQuote: true,
+                    isExactInput: true, // is exact input (base)
+                    amount: partial,
+                    sqrtPriceLimitX96: 0,
+                })
+            })
+
+            it("openNotional--", async () => {
+                const openNotional = await clearingHouse.getOpenNotional(bob.address, baseToken.address)
+                // expect openNotion are same signed
+                expect(openNotional.mul(initOpenNotional).gt(0))
+                expect(openNotional.abs().lt(initOpenNotional.abs())).be.true
+            })
+
+            // problem: it might increase the realized pnl when reducing position
+            it("realizedPnl--", async () => {
+                const pnl = await clearingHouse.getOwedRealizedPnl(bob.address)
+                expect(pnl.lt(0)).be.true
+            })
+        })
+    })
 })
