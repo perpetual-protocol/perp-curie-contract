e0aa31b396596495fc7a7771fe29aefc3f6794ca
kimiwu123 <leonhartwu@gmail.com>
2021-08-03 13:52:29 +0800

    fix: fix the number in comment

--- test/clearingHouse/ClearingHouse.removeLiquidity.with_fee(maker).test.ts
+++ test/clearingHouse/ClearingHouse.removeLiquidity.with_fee(maker).test.ts
@@ -378,7 +378,7 @@ describe("ClearingHouse removeLiquidity with fee", () => {
                 expect(baseBefore.sub(await baseToken.balanceOf(clearingHouse.address))).to.eq(
                     parseEther("0.000816820840785348"),
                 )
-                // 0.000007582881393
+
                 // quote diff: 0.1135501475 (bob swap) - 0.001135501475 (alice removeLiquidity) - 0.112414646 (CH gets from swap) = 2.5E-11
                 expect(quoteBefore.sub(await quoteToken.balanceOf(clearingHouse.address))).to.eq(
                     // 30810663 == 3.0810663E-11
@@ -795,8 +795,7 @@ describe("ClearingHouse removeLiquidity with fee", () => {
                 // base diff:
                 // 0.001625514583 (alice addLiquidity) + 0.000816820841 (carol addLiquidity)
                 // - 0.002281886641 (bob gets (from swap); note the difference)
-                // + 0.002304936001 (bob swap) - (0.000008250715566 * 2 + 0.000006547928875) (alice & carol removeLiquidity)
-                // = 0.002442335424
+                // + 0.002281886641 (bob swap)  = 0.002442335424
                 expect(baseBefore.sub(await baseToken.balanceOf(clearingHouse.address))).to.eq(
                     parseEther("0.002442335423326381"),
                 )
