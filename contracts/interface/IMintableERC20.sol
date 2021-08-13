fa17566f4a51efaac6720ebd82dfd8bebeda9c69
Yuren Ju <yurenju@gmail.com>
2021-08-13 16:49:33 +0800

    chore: add SPDX for all solidity files

--- contracts/interface/IMintableERC20.sol
+++ contracts/interface/IMintableERC20.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.7.6;
 
 import { IERC20Metadata } from "./IERC20Metadata.sol";
--- contracts/test/TestERC20.sol
+++ contracts/test/TestERC20.sol
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.7.6;
 import "@openzeppelin/contracts/presets/ERC20PresetMinterPauser.sol";
 
