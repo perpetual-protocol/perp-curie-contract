1c8ed5245a7af17c84e012744036ed0a411e1a61
Vinta Chen <vinta.chen@gmail.com>
2021-08-13 14:29:42 +0800

    add "prepack" script since package.json doesn't support negative match anymore

---
+++ scripts/prepack.ts
@@ -0,0 +1,16 @@
+import { asyncExec } from "./helper"
+
+async function main(): Promise<void> {
+    await asyncExec("rm -rf artifacts/contracts/test/")
+    await asyncExec("rm -rf artifacts/contracts/uniswap/")
+    await asyncExec("find artifacts/contracts/ -name '*.dbg.json' -delete")
+}
+
+if (require.main === module) {
+    main()
+        .then(() => process.exit(0))
+        .catch(error => {
+            console.error(error)
+            process.exit(1)
+        })
+}
