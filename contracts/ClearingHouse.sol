641cac377fe7c6d82ac7602e7d7846bd5cfe2cde
detoo <daily.detoo@gmail.com>
2021-08-31 01:28:11 -0700

    minor bug fix

--- contracts/ClearingHouse.sol
+++ contracts/ClearingHouse.sol
@@ -872,7 +872,7 @@ contract ClearingHouse is
 
         // CH_EAV: enough account value
         // only cancel open orders if there are not enough free collateral
-        require(_getFreeCollateral(trader) < 0, "CH_EAV");
+        require(_getFreeCollateral(maker) < 0, "CH_EAV");
 
         // must settle funding before getting token info
         _settleFundingAndUpdateFundingGrowth(maker, baseToken);
