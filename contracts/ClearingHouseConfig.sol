1bf69ddb81e46b51db2a7823484e23722d520612
shao <wraecca@gmail.com>
2021-09-13 19:17:36 +0800

    fix ownable init

--- contracts/ClearingHouseConfig.sol
+++ contracts/ClearingHouseConfig.sol
@@ -3,15 +3,12 @@ pragma solidity 0.7.6;
 
 import { SafeOwnable } from "./base/SafeOwnable.sol";
 
-// TODO can be immutable
 contract ClearingHouseConfig is SafeOwnable {
+    uint8 public maxMarketsPerAccount;
     uint24 public imRatio;
     uint24 public mmRatio;
-
     uint24 public liquidationPenaltyRatio;
     uint24 public partialCloseRatio;
-    uint8 public maxMarketsPerAccount;
-
     uint32 public twapInterval;
 
     event TwapIntervalChanged(uint256 twapInterval);
@@ -20,6 +17,8 @@ contract ClearingHouseConfig is SafeOwnable {
     event MaxMarketsPerAccountChanged(uint8 maxMarketsPerAccount);
 
     function initialize() public initializer {
+        __SafeOwnable_init();
+
         imRatio = 10e4; // initial-margin ratio, 10%
         mmRatio = 6.25e4; // minimum-margin ratio, 6.25%
         liquidationPenaltyRatio = 2.5e4; // initial penalty ratio, 2.5%
