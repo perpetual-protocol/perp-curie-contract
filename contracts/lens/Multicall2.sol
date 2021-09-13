d032657ecac7d8c97871f157173d718fea404a86
Paco <liaoishere@gmail.com>
2021-09-13 16:10:35 +0800

    refactor: remove block related functions

--- contracts/lens/Multicall2.sol
+++ contracts/lens/Multicall2.sol
@@ -26,49 +26,6 @@ contract Multicall2 {
         }
     }
 
-    function blockAndAggregate(Call[] memory calls)
-        public
-        returns (
-            uint256 blockNumber,
-            bytes32 blockHash,
-            Result[] memory returnData
-        )
-    {
-        (blockNumber, blockHash, returnData) = tryBlockAndAggregate(true, calls);
-    }
-
-    function getBlockHash(uint256 blockNumber) public view returns (bytes32 blockHash) {
-        blockHash = blockhash(blockNumber);
-    }
-
-    function getBlockNumber() public view returns (uint256 blockNumber) {
-        blockNumber = block.number;
-    }
-
-    function getCurrentBlockCoinbase() public view returns (address coinbase) {
-        coinbase = block.coinbase;
-    }
-
-    function getCurrentBlockDifficulty() public view returns (uint256 difficulty) {
-        difficulty = block.difficulty;
-    }
-
-    function getCurrentBlockGasLimit() public view returns (uint256 gaslimit) {
-        gaslimit = block.gaslimit;
-    }
-
-    function getCurrentBlockTimestamp() public view returns (uint256 timestamp) {
-        timestamp = block.timestamp;
-    }
-
-    function getEthBalance(address addr) public view returns (uint256 balance) {
-        balance = addr.balance;
-    }
-
-    function getLastBlockHash() public view returns (bytes32 blockHash) {
-        blockHash = blockhash(block.number - 1);
-    }
-
     function tryAggregate(bool requireSuccess, Call[] memory calls) public returns (Result[] memory returnData) {
         returnData = new Result[](calls.length);
         for (uint256 i = 0; i < calls.length; i++) {
@@ -80,18 +37,5 @@ contract Multicall2 {
 
             returnData[i] = Result(success, ret);
         }
-    }
-
-    function tryBlockAndAggregate(bool requireSuccess, Call[] memory calls)
-        public
-        returns (
-            uint256 blockNumber,
-            bytes32 blockHash,
-            Result[] memory returnData
-        )
-    {
-        blockNumber = block.number;
-        blockHash = blockhash(block.number);
-        returnData = tryAggregate(requireSuccess, calls);
     }
 }
