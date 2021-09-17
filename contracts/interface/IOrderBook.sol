0f5af18de97896314112fa652809f68bee67b999
boyuchen0224 <boyuchen0224@gmail.com>
2021-09-17 15:24:09 +0800

    Add IOrderBook.sol

---
+++ contracts/interface/IOrderBook.sol
@@ -0,0 +1,130 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
+pragma solidity 0.7.6;
+pragma abicoder v2;
+
+import { Funding } from "../lib/Funding.sol";
+
+interface IOrderBook {
+    event LiquidityChanged(
+        address indexed maker,
+        address indexed baseToken,
+        address indexed quoteToken,
+        int24 lowerTick,
+        int24 upperTick,
+        // amount of base token added to the liquidity (excl. fee) (+: add liquidity, -: remove liquidity)
+        int256 base,
+        // amount of quote token added to the liquidity (excl. fee) (+: add liquidity, -: remove liquidity)
+        int256 quote,
+        int128 liquidity, // amount of liquidity unit added (+: add liquidity, -: remove liquidity)
+        uint256 quoteFee // amount of quote token the maker received as fee
+    );
+
+    struct AddLiquidityParams {
+        address trader;
+        address baseToken;
+        uint256 base;
+        uint256 quote;
+        int24 lowerTick;
+        int24 upperTick;
+        Funding.Growth fundingGrowthGlobal;
+    }
+
+    struct RemoveLiquidityParams {
+        address maker;
+        address baseToken;
+        int24 lowerTick;
+        int24 upperTick;
+        uint128 liquidity;
+    }
+
+    struct AddLiquidityResponse {
+        uint256 base;
+        uint256 quote;
+        uint256 fee;
+        uint128 liquidity;
+    }
+
+    struct RemoveLiquidityResponse {
+        uint256 base;
+        uint256 quote;
+        uint256 fee;
+    }
+
+    /// @param lastFeeGrowthInsideX128 fees in quote token recorded in Exchange
+    ///        because of block-based funding, quote-only and customized fee, all fees are in quote token
+    struct OpenOrder {
+        uint128 liquidity;
+        int24 lowerTick;
+        int24 upperTick;
+        uint256 lastFeeGrowthInsideX128;
+        int256 lastTwPremiumGrowthInsideX96;
+        int256 lastTwPremiumGrowthBelowX96;
+        int256 lastTwPremiumDivBySqrtPriceGrowthInsideX96;
+    }
+
+    struct ReplaySwapParams {
+        address baseToken;
+        bool isBaseToQuote;
+        bool shouldUpdateState;
+        int256 amount;
+        uint160 sqrtPriceLimitX96;
+        uint24 exchangeFeeRatio;
+        uint24 uniswapFeeRatio;
+        Funding.Growth globalFundingGrowth;
+    }
+
+    struct ReplaySwapResponse {
+        int24 tick;
+        uint256 fee; // exchangeFeeRatio
+        uint256 insuranceFundFee; // insuranceFundFee = exchangeFeeRatio * insuranceFundFeeRatio
+    }
+
+    function setExchange(address exchangeArg) external;
+
+    function addLiquidity(AddLiquidityParams calldata params) external returns (AddLiquidityResponse memory);
+
+    function removeLiquidity(RemoveLiquidityParams calldata params) external returns (RemoveLiquidityResponse memory);
+
+    function removeLiquidityByIds(
+        address maker,
+        address baseToken,
+        bytes32[] calldata orderIds
+    ) external returns (RemoveLiquidityResponse memory);
+
+    function updateFundingGrowthAndLiquidityCoefficientInFundingPayment(
+        address trader,
+        address baseToken,
+        Funding.Growth memory fundingGrowthGlobal
+    ) external returns (int256 liquidityCoefficientInFundingPayment);
+
+    function replaySwap(ReplaySwapParams memory params) external returns (ReplaySwapResponse memory);
+
+    function getOpenOrderIds(address trader, address baseToken) external view returns (bytes32[] memory);
+
+    function getOpenOrderById(bytes32 orderId) external view returns (OpenOrder memory);
+
+    function getOpenOrder(
+        address trader,
+        address baseToken,
+        int24 lowerTick,
+        int24 upperTick
+    ) external view returns (OpenOrder memory);
+
+    function hasOrder(address trader, address[] calldata tokens) external view returns (bool);
+
+    function getTotalQuoteAmountInPools(address trader, address[] calldata baseTokens) external view returns (uint256);
+
+    function getTotalTokenAmountInPool(
+        address trader,
+        address baseToken,
+        bool fetchBase
+    ) external view returns (uint256 tokenAmount);
+
+    function getLiquidityCoefficientInFundingPayment(
+        address trader,
+        address baseToken,
+        Funding.Growth memory fundingGrowthGlobal
+    ) external view returns (int256 liquidityCoefficientInFundingPayment);
+
+    function getFeeGrowthGlobal(address baseToken) external view returns (uint256);
+}
