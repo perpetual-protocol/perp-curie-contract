69bf480ce09a7bc049b5f70ad0983b655f57d562
shao <wraecca@gmail.com>
2021-10-02 00:10:57 +0800

    Revert "add VirtualTokenStorage"
    This reverts commit e6a9263f617927cb93d03981ef185835c576abb7.

--- contracts/storage/VirtualTokenStorage.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-pragma solidity 0.7.6;
-
-import { ERC20Upgradeable } from "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-import { SafeOwnable } from "../base/SafeOwnable.sol";
-
-abstract contract VirtualTokenStorageV1 is SafeOwnable, ERC20Upgradeable {
-    mapping(address => bool) internal _whitelistMap;
-}
