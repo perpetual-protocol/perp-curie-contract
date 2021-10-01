cf3d6f05711816b303c8094dcac9bbc19db38395
shao <wraecca@gmail.com>
2021-10-02 00:12:01 +0800

    minor update quoteToken

--- contracts/QuoteToken.sol
+++ contracts/QuoteToken.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 pragma solidity 0.7.6;
-import "./VirtualToken.sol";
+import { VirtualToken } from "./VirtualToken.sol";
 
 contract QuoteToken is VirtualToken {
     function initialize(string memory nameArg, string memory symbolArg) external initializer {
