8827a8b652474759b13c9c700ac87b8ac8482d91
Paco <liaoishere@gmail.com>
2021-10-18 17:18:59 +0800

    use vault.getSettlementToken in helper

--- test/helper/vaultHelper.ts
+++ test/helper/vaultHelper.ts
@@ -1,12 +1,11 @@
-import { BigNumberish, ContractTransaction, ethers, Wallet } from "ethers"
-import { formatUnits, parseEther } from "ethers/lib/utils"
-import { ClearingHouse, OrderBook } from "../../typechain"
+import { ContractTransaction, Wallet } from "ethers"
+import { formatUnits } from "ethers/lib/utils"
 import { ClearingHouseFixture } from "../clearingHouse/fixtures"
 
 export async function withdrawAll(fixture: ClearingHouseFixture, wallet: Wallet): Promise<ContractTransaction> {
     const vault = fixture.vault
     const freeCollateral = await vault.getFreeCollateral(wallet.address)
-    const token = await vault.settlementToken()
+    const token = await vault.getSettlementToken()
     console.log(`freeCollateral=${formatUnits(freeCollateral, 6)}`)
     return vault.connect(wallet).withdraw(token, freeCollateral)
 }
