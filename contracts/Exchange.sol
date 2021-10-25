6ef5b9080778f07e6eae5d27ab6693a8f3633208
bchen <boyuchen0224@gmail.com>
2021-10-25 23:22:16 +0800

    ABDK 32
    - repeated using length on for loop

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -38,6 +38,7 @@ contract Exchange is
     using AddressUpgradeable for address;
     using SafeMathUpgradeable for uint256;
     using SafeMathUpgradeable for uint128;
+    using SafeMathUpgradeable for uint24;
     using SignedSafeMathUpgradeable for int256;
     using PerpMath for uint256;
     using PerpMath for int256;
@@ -107,9 +108,9 @@ contract Exchange is
         // EX_BTNE: base token not exists
         require(IMarketRegistry(_marketRegistry).getPool(baseToken) != address(0), "EX_BTNE");
 
-        // tick range is [-MAX_TICK, MAX_TICK], maxTickCrossedWithinBlock should be in [0, MAX_TICK]
+        // tick range is [-MAX_TICK, MAX_TICK], maxTickCrossedWithinBlock should be in [0, 2 * MAX_TICK]
         // EX_MTCLOOR: max tick crossed limit out of range
-        require(maxTickCrossedWithinBlock <= uint24(TickMath.MAX_TICK), "EX_MTCLOOR");
+        require(maxTickCrossedWithinBlock <= uint24(TickMath.MAX_TICK).mul(2), "EX_MTCLOOR");
 
         _maxTickCrossedWithinBlockMap[baseToken] = maxTickCrossedWithinBlock;
     }
@@ -258,7 +259,9 @@ contract Exchange is
 
     function settleAllFunding(address trader) external override {
         address[] memory baseTokens = IAccountBalance(_accountBalance).getBaseTokens(trader);
-        for (uint256 i = 0; i < baseTokens.length; i++) {
+        uint256 baseTokenLength = baseTokens.length;
+
+        for (uint256 i = 0; i < baseTokenLength; i++) {
             settleFunding(trader, baseTokens[i]);
         }
     }
@@ -378,7 +381,7 @@ contract Exchange is
     /// @dev this function calculates the up-to-date globalFundingGrowth and twaps and pass them out
     /// @return fundingGrowthGlobal the up-to-date globalFundingGrowth
     /// @return markTwap only for _settleFundingAndUpdateFundingGrowth()
-    /// @return indexTwap only for _settleFundingAndUpdateFundingGrowth()
+    /// @return indexTwap only for settleAllFunding()
     function getFundingGrowthGlobalAndTwaps(address baseToken)
         public
         view
