fbf3ba744fed78fb441deb1becd1706619de1be2
kimiwu123 <leonhartwu@gmail.com>
2021-10-25 16:32:33 +0800

    tenderly verify: read from .json instead of .json

--- scripts/tenderly.ts
+++ scripts/tenderly.ts
@@ -9,10 +9,10 @@ interface ContractInfo {
     address: string
 }
 
-function getContractsInfo(network: String, stage: String): Array<ContractInfo> {
+function getContractsInfo(network: String): Array<ContractInfo> {
     const contractsInfo = []
-    const stageMetadata = `./metadata/${stage}.json`
-    const jsonStr = fs.readFileSync(resolve(stageMetadata), "utf8")
+    const metadata = `./metadata/${network}.json`
+    const jsonStr = fs.readFileSync(resolve(metadata), "utf8")
     const { contracts } = JSON.parse(jsonStr)
 
     for (const [name] of Object.entries(contracts)) {
@@ -35,7 +35,7 @@ function getContractsInfo(network: String, stage: String): Array<ContractInfo> {
 
 export async function verifyAndPushContract(hre: HardhatRuntimeEnvironment, stage: String): Promise<void> {
     const network = hre.network.name
-    const contractsInfo = getContractsInfo(network, stage)
+    const contractsInfo = getContractsInfo(network)
 
     for (const { name, address } of contractsInfo) {
         console.log(`verifying contract ${name} on ${address}`)
