2e7a0525e6ec88063749547bc233c3124720e169
detoo <daily.detoo@gmail.com>
2021-10-26 22:40:04 -0700

    fix perpMath tests

--- test/perpMath/perpMath.test.ts
+++ test/perpMath/perpMath.test.ts
@@ -1,9 +1,10 @@
 import { expect } from "chai"
 import { BigNumber } from "ethers"
 import { ethers } from "hardhat"
-describe.only("PerpMath test", async () => {
+describe("PerpMath test", async () => {
     const x96 = BigNumber.from(2).pow(96)
     const x10_18 = BigNumber.from(10).pow(18)
+    const x10_6 = BigNumber.from(10).pow(6)
     const maxUint256 = BigNumber.from(2).pow(256).sub(1)
     const maxUint160 = BigNumber.from(2).pow(160).sub(1)
     const maxInt256 = BigNumber.from(2).pow(255).sub(1)
@@ -21,7 +22,8 @@ describe.only("PerpMath test", async () => {
     })
 
     it("formatX10_18ToX96", async () => {
-        expect(await perpMath.testFormatX10_18ToX96(maxUint256)).to.be.deep.eq(maxUint256.mul(x96).div(x10_18))
+        const value = maxUint256.mul(x10_18).div(x96) // per on FullMath.mulDiv() specs, max input without overflow
+        expect(await perpMath.testFormatX10_18ToX96(value)).to.be.deep.eq(value.mul(x96).div(x10_18))
     })
 
     it("formatX96ToX10_18", async () => {
@@ -29,14 +31,14 @@ describe.only("PerpMath test", async () => {
     })
 
     it("max", async () => {
-        const a = BigNumber.from(2).pow(256).sub(1)
-        const b = BigNumber.from(2).pow(256).sub(2)
+        const a = maxInt256.sub(1)
+        const b = maxInt256.sub(2)
         expect(await perpMath.testMax(a, b)).to.be.deep.eq(a)
     })
 
     it("min", async () => {
-        const a = BigNumber.from(2).pow(256).sub(1)
-        const b = BigNumber.from(2).pow(256).sub(2)
+        const a = maxInt256.sub(1)
+        const b = maxInt256.sub(2)
         expect(await perpMath.testMin(a, b)).to.be.deep.eq(b)
     })
 
@@ -45,21 +47,22 @@ describe.only("PerpMath test", async () => {
     })
 
     it("force error, abs negative overflow", async () => {
-        expect(await perpMath.testAbs(minInt256)).to.reverted("")
+        // TODO WIP pending PR for negative overflow
+        await expect(perpMath.testAbs(minInt256)).to.be.revertedWith("SafeCast: value must be positive")
     })
 
     it("divBy10_18 int", async () => {
-        expect(await perpMath.functions["testDivBy10_18(int256)"](maxInt256)).to.be.deep.eq(maxInt256.div(x10_18))
+        expect(await perpMath["testDivBy10_18(int256)"](maxInt256)).to.be.deep.eq(maxInt256.div(x10_18))
     })
 
     it("divBy10_18 uint", async () => {
-        expect(await perpMath.functions["testDivBy10_18(uint256)"](maxUint256)).to.be.deep.eq(maxUint256.div(x10_18))
+        expect(await perpMath["testDivBy10_18(uint256)"](maxUint256)).to.be.deep.eq(maxUint256.div(x10_18))
     })
 
     it("mulRatio", async () => {
-        const value = BigNumber.from(2).pow(256).sub(1)
-        const ratio = BigNumber.from(2).pow(24).sub(1)
-        const e6 = BigNumber.from(1).pow(24)
-        expect(await perpMath.testMulRatio(value, ratio)).to.be.deep.eq(value.mul(ratio).div(e6))
+        // per on FullMath.mulDiv() specs, max input without overflow
+        const value = BigNumber.from(2).pow(256).sub(1).div(2)
+        const ratio = x10_6.mul(2)
+        expect(await perpMath.testMulRatio(value, ratio)).to.be.deep.eq(value.mul(ratio).div(x10_6))
     })
 })
