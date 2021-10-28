55b5657dc1a8c2e67270d6f0b796e40047314475
Miya Chen <miyatlchen@gmail.com>
2021-10-28 16:25:06 +0800

    fix: abdk audit 199,201, add comments
    Co-authored-by: Paco <liaoishere@gmail.com>

--- contracts/interface/IPriceFeed.sol
+++ contracts/interface/IPriceFeed.sol
@@ -4,5 +4,7 @@ pragma solidity 0.7.6;
 interface IPriceFeed {
     function decimals() external view returns (uint8);
 
+    /// @dev Returns the index price of the token.
+    /// @param interval The interval represents twap interval.
     function getPrice(uint256 interval) external view returns (uint256);
 }
