e7d37fdc738c92918305da76e0ba74ad03b028d4
wraecca <wraecca@gmail.com>
2021-10-31 23:49:07 +0800

    use @perp/perp-oracle-contract package

--- contracts/BaseToken.sol
+++ contracts/BaseToken.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.7.6;
 
 import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
-import { IPriceFeed } from "./interface/IPriceFeed.sol";
+import { IPriceFeed } from "@perp/perp-oracle-contract/contracts/interface/IPriceFeed.sol";
 import { IIndexPrice } from "./interface/IIndexPrice.sol";
 import { VirtualToken } from "./VirtualToken.sol";
 import { BaseTokenStorageV1 } from "./storage/BaseTokenStorage.sol";
