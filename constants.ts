67e6a208f065bce9487eb28093ccdd89e4957019
bchen <boyuchen0224@gmail.com>
2021-11-24 02:06:53 +0800

    Fixed _.defaultTo issue

--- constants.ts
+++ constants.ts
@@ -5,12 +5,13 @@ export const ARBITRUM_RINKEBY_DEPLOYER_MNEMONIC = _.defaultTo(
     `${process.env["ARBITRUM_RINKEBY_DEPLOYER_MNEMONIC"]}`,
     "",
 )
-export const ARBITRUM_RINKEBY_WEB3_ENDPOINT = _.defaultTo(`${process.env["ARBITRUM_RINKEBY_WEB3_ENDPOINT"]}`, "")
-export const OPTIMISM_KOVAN_WEB3_ENDPOINT = _.defaultTo(`${process.env["OPTIMISM_KOVAN_WEB3_ENDPOINT"]}`, "")
-export const OPTIMISM_KOVAN_DEPLOYER_MNEMONIC = _.defaultTo(`${process.env["OPTIMISM_KOVAN_DEPLOYER_MNEMONIC"]}`, "")
-export const OPTIMISM_WEB3_ENDPOINT = _.defaultTo(`${process.env["OPTIMISM_WEB3_ENDPOINT"]}`, "")
-export const OPTIMISM_DEPLOYER_MNEMONIC = _.defaultTo(`${process.env["OPTIMISM_DEPLOYER_MNEMONIC"]}`, "")
-export const STAGE = _.defaultTo(`${process.env["STAGE"]}`, "")
+export const ARBITRUM_RINKEBY_WEB3_ENDPOINT = _.defaultTo(process.env["ARBITRUM_RINKEBY_WEB3_ENDPOINT"], "")
+export const OPTIMISM_KOVAN_WEB3_ENDPOINT = _.defaultTo(process.env["OPTIMISM_KOVAN_WEB3_ENDPOINT"], "")
+export const OPTIMISM_KOVAN_DEPLOYER_MNEMONIC = _.defaultTo(process.env["OPTIMISM_KOVAN_DEPLOYER_MNEMONIC"], "")
+export const OPTIMISM_WEB3_ENDPOINT = _.defaultTo(process.env["OPTIMISM_WEB3_ENDPOINT"], "")
+export const OPTIMISM_DEPLOYER_MNEMONIC = _.defaultTo(process.env["OPTIMISM_DEPLOYER_MNEMONIC"], "")
+export const COMPANION_NETWORK = _.defaultTo(process.env["COMPANION_NETWORK"], "")
+
 if (_.isEmpty(RINKEBY_DEPLOYER_MNEMONIC)) {
     console.warn("RINKEBY_MNEMONIC is empty")
 }
@@ -35,6 +36,6 @@ if (_.isEmpty(OPTIMISM_DEPLOYER_MNEMONIC)) {
 if (_.isEmpty(OPTIMISM_WEB3_ENDPOINT)) {
     console.warn("OPTIMISM_WEB3_ENDPOINT is empty")
 }
-if (_.isEmpty(STAGE)) {
-    console.warn("STAGE is empty")
+if (_.isEmpty(COMPANION_NETWORK)) {
+    console.warn("COMPANION_NETWORK is empty")
 }
