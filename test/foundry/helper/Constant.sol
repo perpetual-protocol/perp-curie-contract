6b8f61ede012f480dcd65bc9b5793e2ea54aba6a
miyachen <miyatlchen@gmail.com>
2024-08-12 00:25:38 +0800

    fix pragma abicoder v2

--- test/foundry/helper/Constant.sol
+++ test/foundry/helper/Constant.sol
@@ -1,4 +1,5 @@
 pragma solidity 0.7.6;
+pragma abicoder v2;
 
 import "forge-std/Test.sol";
 
--- test/foundry/immunefi/34300.InvalidWithdrawal.t.sol
+++ test/foundry/immunefi/34300.InvalidWithdrawal.t.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.7.6;
+pragma abicoder v2;
 
 import "forge-std/Test.sol";
 import "../../../contracts/Vault.sol";
